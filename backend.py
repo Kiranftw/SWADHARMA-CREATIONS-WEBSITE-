0cd25a01706a6f858ba007d5fffd3a6855134d61
UDAY KIRAN <uday33184@gmail.com>
2025-05-15 16:44:47 +0530

    created new case sstudy page and linked it to landingpage.html

--- backend.py
+++ backend.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def landing_page():
     return render_template("Landingpage.html")
 
+@app.route("/casestudy")
+def case_study():
+    return render_template("CantactUs.html")
+
 if __name__ == "__main__":
     app.run(debug=True, port=8000)
